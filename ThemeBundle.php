2d73b3bc0631ca9d7a50f1b4e387bd3f085a5f07
Manuel Sanchís <msanchis@edemy.es>
2015-07-15 12:39:17 +0200

    beta

--- ThemeBundle.php
+++ ThemeBundle.php
@@ -6,4 +6,26 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class eDemyThemeBundle extends Bundle
 {
+    public static function getBundleName($type = null)
+    {
+        if ($type == null) {
+
+            return 'eDemyThemeBundle';
+        } else {
+            if ($type == 'Simple') {
+
+                return 'Theme';
+            } else {
+                if ($type == 'simple') {
+
+                    return 'theme';
+                }
+            }
+        }
+    }
+
+    public static function eDemyBundle() {
+
+        return true;
+    }
 }
